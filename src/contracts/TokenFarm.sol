ae10fccfec47fedc4522d4c2e5268ec4e26b584d
Parsa Hosseini <officer47p@gmail.com>
2021-12-03 13:59:38 +0330

    deploy Dai and DappTokens to blockchain.

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,8 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
     // All code goes here...
-
     string public name = "Dapp Token Farm";
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
